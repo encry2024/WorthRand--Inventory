3df3004e629c110bfcd940a12e9f39d599e08c07
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2017-07-31 12:45:42 +0800

    Aftermarket filesize upload removed

--- app/Http/Controllers/Admin/ItemController.php
+++ app/Http/Controllers/Admin/ItemController.php
@@ -347,7 +347,6 @@ class ItemController extends Controller
       header('Content-type: application/pdf');
       header('Content-Disposition: inline; filename="' . $filename . '"');
       header('Content-Transfer-Encoding: binary');
-      header('Content-Length: ' . filesize($file));
       header('Accept-Ranges: bytes');
 
       @readfile($file);
